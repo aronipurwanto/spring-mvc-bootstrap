b3054f955645b8c0cfc2f08ec21e215d202c4086
roni.purwanto@amartha.com <roni.purwanto@amartha.com>
2024-08-03 15:14:13 +0700

    feat: fix detail

--- src/main/java/com/gentara/mvcbootstrap/controller/StudentController.java
+++ src/main/java/com/gentara/mvcbootstrap/controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.gentara.mvcbootstrap.controller;
 
+import com.gentara.mvcbootstrap.model.SchoolClassRes;
+import com.gentara.mvcbootstrap.model.SchoolHistoryRes;
 import com.gentara.mvcbootstrap.model.StudentRes;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,8 +23,45 @@ public class StudentController {
     // constructor
     public StudentController(List<StudentRes> studentResList) {
         this.studentResList = new ArrayList<>();
+        List<SchoolHistoryRes> roniSchoolHistoryResList = new ArrayList<>();
+        roniSchoolHistoryResList.add(
+                new SchoolHistoryRes(11,"SDN 1 Kecubung Raya","Kecubung Raya", "Menggala",
+                        Arrays.asList(
+                                new SchoolClassRes(111,"Kelas 1", LocalDate.of(1991,7,16), LocalDate.of(1992,6,30),"Ibu Anu"),
+                                new SchoolClassRes(112,"Kelas 2", LocalDate.of(1992,7,16), LocalDate.of(1993,6,30),"Ibu Anu"),
+                                new SchoolClassRes(113,"Kelas 3", LocalDate.of(1993,7,16), LocalDate.of(1994,6,30),"Ibu Anu"),
+                                new SchoolClassRes(114,"Kelas 4", LocalDate.of(1994,7,16), LocalDate.of(1995,6,30),"Ibu Anu"),
+                                new SchoolClassRes(115,"Kelas 5", LocalDate.of(1996,7,16), LocalDate.of(1997,6,30),"Ibu Anu"),
+                                new SchoolClassRes(116,"Kelas 6", LocalDate.of(1997,7,16), LocalDate.of(1998,6,30),"Ibu Anu")
+                        )
+                )
+        );
+
+        roniSchoolHistoryResList.add(
+                new SchoolHistoryRes(12,"SMP 1 Patian Rowo","Patianrowo", "Nganjuk",
+                        Arrays.asList(
+                                new SchoolClassRes(121,"Kelas 1", LocalDate.of(1998,7,16), LocalDate.of(1999,6,30),"Ibu Anu"),
+                                new SchoolClassRes(122,"Kelas 2", LocalDate.of(1999,7,16), LocalDate.of(2000,6,30),"Ibu Anu"),
+                                new SchoolClassRes(133,"Kelas 3", LocalDate.of(2000,7,16), LocalDate.of(2001,6,30),"Ibu Anu")
+                        )
+                )
+        );
+
+        roniSchoolHistoryResList.add(
+                new SchoolHistoryRes(13,"SMA N 1 Kertosono","Kertosono", "Nganjuk",
+                        Arrays.asList(
+                                new SchoolClassRes(131,"Kelas 1", LocalDate.of(2001,7,16), LocalDate.of(2002,6,30),"Ibu Anu"),
+                                new SchoolClassRes(132,"Kelas 2", LocalDate.of(2002,7,16), LocalDate.of(2003,6,30),"Ibu Anu"),
+                                new SchoolClassRes(133,"Kelas 3", LocalDate.of(2004,7,16), LocalDate.of(2005,6,30),"Ibu Anu")
+                        )
+                )
+        );
+
+        StudentRes roni = new StudentRes(1, "Ahmad", "Roni", "Ciamis");
+        roni.setSchoolHistory(roniSchoolHistoryResList);
+
         // generate data
-        this.studentResList.add(new StudentRes(1,"Ahmad","Roni","Ciamis"));
+        this.studentResList.add(roni);
         this.studentResList.add(new StudentRes(2,"Ahmad","Sobirin","Ciamis"));
         this.studentResList.add(new StudentRes(3,"Ahmad","Sabilla","Ciamis"));
         this.studentResList.add(new StudentRes(4,"Ahmad","Arifin","Ciamis"));
@@ -48,7 +89,7 @@ public class StudentController {
          // cara 2
         Optional<StudentRes> detail = studentResList.stream().filter(x -> x.getId() == id).findFirst();
         if (detail.isPresent()) {
-            modelAndView.addObject("studentRes", detail.get());
+            modelAndView.addObject("student", detail.get());
         } else {
             return new ModelAndView("redirect:/student");
         }
--- src/main/java/com/gentara/mvcbootstrap/model/SchoolClassRes.java
+++ src/main/java/com/gentara/mvcbootstrap/model/SchoolClassRes.java
@@ -1,10 +1,14 @@
 package com.gentara.mvcbootstrap.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SchoolClassRes {
     private int id;
     private String schoolClass;
